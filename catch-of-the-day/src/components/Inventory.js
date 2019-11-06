384ff0f8d3a099d7e7abe58b872ab13d08598e35
David Wright <David.Wright@thewalkergroup.ca>
2019-11-05 20:26:24 -0800

    finished 6/29

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+  render() {
+    return <div className="inventory">inventory</div>;
+  }
+}
+
+export default Inventory;
